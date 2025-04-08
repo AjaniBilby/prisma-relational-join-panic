312eef359dea54f1adb8349fe18ebb2db33dcb67
Ajani Bilby <me@ajanibilby.com>
2025-04-08 13:35:07 +1000

    +init

---
+++ test.js
@@ -0,0 +1,38 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+function getClient() {
+	const databaseUrl = new URL("postgresql://postgres:postgres@localhost:5432/prisma-relational-join-panic");
+	console.info(`🔌 setting up prisma client to ${databaseUrl.host}`);
+	const client = new PrismaClient({
+		datasources: {
+			db: {
+				url: databaseUrl.toString(),
+			},
+		},
+	});
+	// connect eagerly
+	client.$connect();
+
+	return client;
+}
+
+const prisma = getClient();
+
+
+async function Read(orderID) {
+	return await prisma.thing.findUnique({
+		select: { id: true },
+		where:  { id: orderID },
+		relationLoadStrategy: "join" // remove this and there is no panic
+	});
+}
+
+
+async function run() {
+	const set = [1,2]; // reduce to one item and there is no panic
+
+	const results = await Promise.all(set.map(Read));
+	console.log(results);
+}
+
+run();
